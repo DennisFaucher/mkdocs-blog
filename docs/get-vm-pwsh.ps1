5e5b7295ed5ac92c539474c56b901b2cdfd0e030
Dennis Faucher <dennis@faucher.net>
2023-09-18 09:19:51 -0400

    Delete docs/get-vm-pwsh.ps1

--- docs/get-vm-pwsh.ps1
+++
@@ -1,4 +0,0 @@
-Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false
-$Password = Get-Content "/home/dennis/Documents/MkDocs/vcenter-mkdocs/docs/clear_password.txt"
-Connect-viserver -server 192.168.1.55 -user administrator@vsphere.local -password $Password
-Get-VM |select Name,NumCpu,MemoryGB,PowerState | Format-Table >> /home/dennis/Documents/MkDocs/vcenter-mkdocs/docs/index.md
